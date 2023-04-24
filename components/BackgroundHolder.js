b8f025a04d3374bdd1a1bb1a78211fab2ea232fe
dykmalon <dykmalon@gmail.com>
2023-04-24 12:07:07 +0530

    holder dapp modify

--- components/BackgroundHolder.js
+++ components/BackgroundHolder.js
@@ -3,45 +3,46 @@ import Image from 'next/image';
 
 
 export default function BackgroundHolder() {
+
     return (
-    <>
+        <>
 
-        <div className="public-bg">
+            <div className="public-bg">
 
-            <div>
-                <Image src="/assets/background.png" layout="fill" objectFit="containe" alt="Image description" />
-            </div>
-
-            <div>
-                <Image src="/assets/wires.png" layout="fill" objectFit="containe" alt="Image description" />
-            </div>
-
-            <div>
-                <Image src="/assets/pods.png" layout="fill" objectFit="containe" alt="Image description" />
-            </div>
-
-            <div>
-                <Image src="/assets/hmachine.png" layout="fill" objectFit="containe" alt="Image description" />
-            </div>
-
-        </div>
-
-        <div className="public-pods">
-
-                    <div>
-                        <Image src="/assets/allpods.png" layout="fill" objectFit="containe" alt="Image description" />
-                    </div>
-
-                    <div>
-                        <Image src="/assets/left-floater.gif" layout="fill" objectFit="containe" alt="Image description" />
-                    </div>
-
-                    <div>
-                        <Image src="/assets/right-floater.gif" layout="fill" objectFit="containe" alt="Image description" />
-                    </div>
-
+                <div>
+                    <Image src="/assets/background.png" layout="fill" objectFit="containe" alt="Image description" />
                 </div>
 
-    </>
+                <div>
+                    <Image src="/assets/wires.png" layout="fill" objectFit="containe" alt="Image description" />
+                </div>
+
+                <div>
+                    <Image src="/assets/pods.png" layout="fill" objectFit="containe" alt="Image description" />
+                </div>
+
+                <div style={{ top: '-4px'}}>
+                    <Image src="/assets/hmachine.png" layout="fill" objectFit="containe" alt="Image description"   />
+                </div>
+
+            </div>
+
+            <div className="public-pods">
+
+                <div>
+                    <Image src="/assets/allpods.png" layout="fill" objectFit="containe" alt="Image description" />
+                </div>
+
+                <div>
+                    <Image src="/assets/left-floater.gif" layout="fill" objectFit="containe" alt="Image description" />
+                </div>
+
+                <div>
+                    <Image src="/assets/right-floater.gif" layout="fill" objectFit="containe" alt="Image description" />
+                </div>
+
+            </div>
+
+        </>
     )
 }
